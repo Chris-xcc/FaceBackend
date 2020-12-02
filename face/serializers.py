052ff6b71a1e3ac9c9d0097a4b886310974231bb
Chris-xcc <chris_guoc@163.com>
2020-12-02 19:10:40 +0800

    modify upload face

--- face/serializers.py
+++ face/serializers.py
@@ -1,14 +1,11 @@
 import cv2
-from time import localtime, strftime
-from random import randint
-
-import numpy as np
 from PIL import Image
 from django.core.validators import FileExtensionValidator
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 
-from utils.image_processing import detector_dlib, detector_cv2, image_to_np
+from utils.image_processing import detector_cv2, image_to_np, image_to_io, img_name, base64_to_cv2, sign_img_save_path, \
+    user_face_file
 from .models import User, Sign
 
 
@@ -16,61 +13,82 @@ class UserDetailSerializer(serializers.ModelSerializer):
     """
     用户详情序列化类
     """
-    SEX_TYPE = (
-        ('male', '男'),
-        ('female', '女'),
-    )
-    username = serializers.CharField(required=False, label='姓名', help_text='姓名', max_length=20,
-                                     validators=[UniqueValidator(queryset=User.objects.all(), message='该用户已存在')])
-    sex = serializers.ChoiceField(required=False, label='性别', help_text='性别', choices=SEX_TYPE)
-    password = serializers.CharField(required=False, label='密码', help_text='密码', write_only=True)
+    # SEX_TYPE = (
+    #     ('male', '男'),
+    #     ('female', '女'),
+    # )
+    # username = serializers.CharField(required=False, label='姓名', help_text='姓名', max_length=20,
+    #                                  validators=[UniqueValidator(queryset=User.objects.all(), message='该用户已存在')])
+    # sex = serializers.CharField(required=False, label='性别', help_text='性别', read_only=True)
+    new_password = serializers.CharField(required=False, label='新密码', help_text='新密码', write_only=True)
     check_password = serializers.CharField(required=False, label='确认密码', help_text='确认密码', write_only=True)
-    face = serializers.ImageField(required=False, label='人脸图片', help_text='人脸图片',
-                                  validators=[FileExtensionValidator(['jpg', 'png'], message='必须为jpg,png格式的文件')])
+    face = serializers.CharField(required=False, label='人脸图片', help_text='人脸图片', )
+
+    # validators=[FileExtensionValidator(['jpg', 'png'], message='必须为jpg,png格式的文件')])
 
     def update(self, instance, validated_data):
         """更新，instance为要更新的对象实例"""
-        username = validated_data.get('username', instance.username)
-        sex = validated_data.get('sex', instance.sex)
-        password = validated_data.get('password', instance.password)
+        # username = validated_data.get('username', instance.username)
+        # sex = validated_data.get('sex', instance.sex)
+        new_password = validated_data.get('new_password', instance.password)
         check_password = validated_data.get('check_password', instance.check_password)
         face = validated_data.get('face', instance.face)
-        if face:
-            image_np = image_to_np(face)
-            faces = detector_dlib(image_np, 0)
-            for index, rect in enumerate(faces):
-                cv2.rectangle(image_np, (rect.top(), rect.left()), (rect.bottom(), rect.right()), (0, 0, 225), 2)
+        img = base64_to_cv2(face)
+        img_path = (sign_img_save_path.joinpath(instance.number).joinpath(img_name))
 
-            if len(faces) == 1:
-                pass
-            else:
-                raise serializers.ValidationError('图片不符合要求')
-            img_name = strftime("%Y%m%d%H%M%S", localtime()) + str(randint(10, 99)) + '.jpg'
-            face._name = img_name
-            instance.face = face
-        if password or check_password == '':
+        # 数据库保存目录
+        # face_img / 2017127290 / 2020120121133629.jpg
+        db_path = '/'.join(img_path.parts[-3:])
+
+        # 创建用户人脸目录
+        user_face_file(instance.number)
+        # print(db_path)
+        # print(img_path)
+        cv2.imwrite(str(img_path), img)
+        # print(image)
+        # face_img / 2017127249 / 微信图片_20201125224726.jpg
+        instance.face = db_path
+        # print(instance.face)
+        if new_password == check_password:
+            instance.set_password(new_password)
+        instance.save()
+
+        return instance
+
+    def validate_face(self, face):
+        # base64人脸图片
+        image = base64_to_cv2(face)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        faces = detector_cv2.detectMultiScale(gray, 1.2, 5)
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(gray, (x, y), (x + w, y + h), (0, 0, 225), 2)
+
+        if len(faces) == 1:
             pass
         else:
-            if password == check_password:
-                instance.set_password(password)
-            else:
-                raise serializers.ValidationError('密码不一致')
-        instance.save()
-        return instance
+            raise serializers.ValidationError('图片不符合要求')
+
+        return face
 
     class Meta:
         model = User
         fields = [
             'id',
             'username',
-            'password',
-            'number',
+            'new_password',
             'sex',
+            'number',
             'check_password',
-            'face'
+            'face',
+            'is_superuser'
         ]
         read_only_fields = [
+            'username',
             'number',
+            'is_superuser',
+            'sex',
         ]
 
 
@@ -79,8 +97,8 @@ class UserRegisterSerializer(serializers.Serializer):
         ('male', '男'),
         ('female', '女'),
     )
-    # username = serializers.CharField(required=True, label='姓名', help_text='姓名', max_length=20,
-    #                                  validators=[UniqueValidator(queryset=User.objects.all(), message='该用户已存在')])
+    username = serializers.CharField(required=True, label='姓名', help_text='姓名', max_length=20,
+                                     validators=[UniqueValidator(queryset=User.objects.all(), message='该用户已存在')])
     number = serializers.CharField(required=True, label='学号', help_text='学号', max_length=20, min_length=7,
                                    validators=[UniqueValidator(queryset=User.objects.all(), message='学号已存在')])
     password = serializers.CharField(required=True, label='密码', help_text='密码', max_length=14, min_length=6,
@@ -88,7 +106,7 @@ class UserRegisterSerializer(serializers.Serializer):
     check_password = serializers.CharField(required=True, label='确认密码', help_text='确认密码',
                                            max_length=14, min_length=6, write_only=True)
 
-    # sex = serializers.ChoiceField(required=True, label='性别', help_text='性别', choices=SEX_TYPE)
+    sex = serializers.ChoiceField(required=True, label='性别', help_text='性别', choices=SEX_TYPE)
 
     def validate(self, attrs):
         """
@@ -103,11 +121,11 @@ class UserRegisterSerializer(serializers.Serializer):
         return attrs
 
     def create(self, validated_data):
-        # username = validated_data['username']
+        username = validated_data['username']
         number = validated_data['number']
         password = validated_data['password']
-        # sex = validated_data['sex']
-        user = User.objects.create(number=number, password=password)
+        sex = validated_data['sex']
+        user = User.objects.create(number=number, password=password, sex=sex, username=username)
 
         # 设置用户密码
         user.set_password(password)
@@ -122,21 +140,38 @@ class SignCreateSerializer(serializers.ModelSerializer):
 
 
 class StuSignSerializer(serializers.ModelSerializer):
-    face = serializers.ImageField(required=False, label='人脸图片', help_text='人脸图片',
-                                  max_length=None, use_url=True, allow_empty_file=False,
-                                  validators=[FileExtensionValidator(['jpg', 'png'], message='必须为jpg,png格式的文件')])
+    face = serializers.CharField(required=False, label='人脸图片', help_text='人脸图片', )
 
+    # max_length=None, use_url=True, allow_empty_file=False,
+    # validators=[FileExtensionValidator(['jpg', 'png','jpeg'], message='必须为jpg,png格式的文件')])
     def validate(self, attrs):
+        face_base64 = attrs['face']
+        image = image_to_io(face_base64)
+        image_np = image_to_np(image)
 
-        # 图片
-        face = attrs['face']
-        image_np = image_to_np(face)
         faces = detector_cv2.detectMultiScale(image_np, 1.2, 5)
         # 人脸数量
         if len(faces) == 1:
             return attrs
         else:
             raise serializers.ValidationError('图片无效')
+
+    # def validate(self, attrs):
+    # 图片
+    # face = attrs['face'].file
+    # print(face.__dict__)
+    # image = Image.open(face.file).convert('L')
+    # # 将图片样本转换为ndarray类型
+    # image_np = np.array(image, 'uint8')
+    # cv2.imshow('1',image_np)
+    # cv2.waitKey(0)
+    #     image_np = image_to_np(face)
+    #     faces = detector_cv2.detectMultiScale(image_np, 1.2, 5)
+    #     # 人脸数量
+    #     if len(faces) == 1:
+    #         return attrs
+    #     else:
+    #         raise serializers.ValidationError('图片无效')
 
     class Meta:
         model = Sign
