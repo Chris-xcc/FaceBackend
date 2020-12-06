75d1d515b762a0644e04139bcbd5ba2d43b1d8b1
Chris-xcc <chris_guoc@163.com>
2020-12-06 18:08:14 +0800

    run in uvicorn

---
+++ script/run_uvicorn.py
@@ -0,0 +1,20 @@
+import os
+import uvicorn
+
+
+def main():
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+    uvicorn.run(
+        "backend.asgi:application",
+        host="0.0.0.0",
+        port=8000,
+        log_level="debug",
+        reload=True,
+    )
+
+
+if __name__ == "__main__":
+    main()
+# gunicorn example:app -w 4 -k uvicorn.workers.UvicornWorker
+# gunicorn myblog.asgi:application -w 4 -k uvicorn.workers.UvicornWorker
+# gunicorn -w 4 -k uvicorn.workers.UvicornH11Worker
